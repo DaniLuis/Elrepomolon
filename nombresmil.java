fc2d520a9c2000f4185aa517586e3ba0630f7072
Luishiko <Luishiko>
2014-09-11 11:13:21 +0200

    Más código

--- nombresmil.java
+++ nombresmil.java
@@ -1,1 +1,17 @@
-penerabofalopalotrankapicha
+import java.util.Scanner;
+public class Nombresmil{
+	public static String anyade(){
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Añade un sinónimo: ");
+		String palabra = teclado.nextLine();
+
+		return palabra;
+	}
+	public static void main (String args[]){
+		String palabra;
+		while(true){
+			palabra = anyade();
+			System.out.println("penerabofalopalotrankapicha"+palabra);
+		}
+	}
+}
